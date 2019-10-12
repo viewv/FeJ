62f199e039010971cc7c852127d6be5e4c18a5db
viewv <zxnnet@gmail.com>
2019-10-12 10:06:24 +0800

    Add H2 DataBase and fix icon bugs by change into de 8.1

--- src/main/java/top/viewv/controller/MainApp.java
+++ src/main/java/top/viewv/controller/MainApp.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.util.Objects;
 
--- src/main/java/top/viewv/test/TestConnection.java
+++ src/main/java/top/viewv/test/TestConnection.java
@@ -6,12 +6,12 @@ public class TestConnection {
 
     // MySQL 8.0 以下版本 - JDBC 驱动名及数据库 URL
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://rm-wz9sa6hd1097u6rx4bo.mysql.rds.aliyuncs.com:3306/mysql";
+    static final String DB_URL = "jdbc:mysql://rm-wz9sa6hd1097u6rx4bo.mysql.rds.aliyuncs.com:3306/fems";
 
     // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
     //static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     //static final String DB_URL = "jdbc:mysql://localhost:3306/RUNOOB?useSSL=false&serverTimezone=UTC";
-    
+
     // 数据库的用户名与密码，需要根据自己的设置
     static final String USER = "root";
     static final String PASS = "Ingram14";
@@ -31,27 +31,29 @@ public class TestConnection {
             System.out.println(" 实例化Statement对象...");
             stmt = conn.createStatement();
             String sql;
-            sql = "SELECT User FROM user";
+            sql = "SELECT account_id,password FROM account";
             ResultSet rs = stmt.executeQuery(sql);
 
             while(rs.next()){
                 // 通过字段检索
-                String id  = rs.getString("User");
+                String id  = rs.getString("account_id");
+                String password = rs.getString("password");
+
 
                 // 输出数据
                 System.out.println("ID: " + id);
+                System.out.println("Password: "+ password);
+
             }
             // 完成后关闭
             rs.close();
             stmt.close();
             conn.close();
-        }catch(SQLException se){
+        } catch(Exception se){
             // 处理 JDBC 错误
             se.printStackTrace();
-        }catch(Exception e){
-            // 处理 Class.forName 错误
-            e.printStackTrace();
-        }finally{
+        }// 处理 Class.forName 错误
+        finally{
             // 关闭资源
             try{
                 if(stmt!=null) stmt.close();
