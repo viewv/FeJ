5277ebbfdafc897d1cb23539f712835ab09fded6
viewv <zxnnet@gmail.com>
2019-10-14 17:14:59 +0800

    JFx 11 is really a problem, Change into 8 add reg

--- src/main/java/top/viewv/controller/MainController.java
+++ src/main/java/top/viewv/controller/MainController.java
@@ -1,107 +1,90 @@
 package top.viewv.controller;
 
 import com.jfoenix.controls.*;
-import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import java.net.URL;
-import java.sql.PreparedStatement;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.util.Duration;
 import top.viewv.database.Login;
-import top.viewv.view.StageManager;
 
 
 public class MainController implements Initializable {
 
-    @FXML Button btnsignup;
-    @FXML Button enterButton;
-    @FXML TextField userInput;
-    @FXML TextField passwordInput;
-    @FXML AnchorPane loginPane;
-    @FXML HBox BottomPane;
-    @FXML StackPane InputPane;
-
-
-
-
+    @FXML
+    Button enterButton;
+    @FXML
+    TextField userInput;
+    @FXML
+    TextField passwordInput;
+    @FXML
+    AnchorPane loginPane;
+    @FXML
+    HBox BottomPane;
+    @FXML
+    StackPane InputPane;
+    @FXML
+    Group GroupSignup;
+    @FXML
+    Group GroupLogin;
+    @FXML
+    Button regButton;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb){
-        try {
-//            JFXSnackbar snackbar = new JFXSnackbar(loginPane);
+    public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("FeJ Open");
 
-//            snackbar.fireEvent(new JFXSnackbar.SnackbarEvent(
-//                    new JFXSnackbarLayout("Snackbar Message Persistent Jsdjsidjdnsdisjdiadid " ,
-//                            "CLOSE",
-//                            action -> snackbar.close()),
-//                    Duration.seconds(10), null));
-
-            System.out.println("FeJ Open");
-            String unselectedColor = "#FFFFFF";
-            String selectedColor = "#aeaeae";
-            int LogOrReg = 0; //0 for log 1 for reg
-
-        }catch (Exception ex){
-            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE,null,ex);
-        }
+        GroupLogin.setVisible(true);
+        GroupSignup.setVisible(false);
     }
-
-//    private void loadDialog(String message){
-//        JFXDialogLayout context = new JFXDialogLayout();
-//        context.setHeading(new Text("Heading"));
-//        context.setBody(new Text(message));
-//        JFXDialog dialog = new JFXDialog(InputPane,context,JFXDialog.DialogTransition.CENTER);
-//        JFXButton button = new JFXButton("Okey");
-//        button.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent event) {
-//                dialog.close();
-//            }
-//        });
-//        context.setActions(button);
-//        dialog.show();
-//    }
 
     @FXML
     public void GetInput(MouseEvent mouseEvent) {
+
         String user_id = userInput.getText().trim();
         String password = passwordInput.getText().trim();
         Login login = new Login();
 
-        String result = login.LoginFun(user_id,password);
-        String icon ;
+        String result = login.LoginFun(user_id, password);
+        String icon;
 
-        if (result != null){
-            if (result == "account_error"){
+
+        if (result != null) {
+            if (result == "account_error") {
                 System.out.println(result);
 
-            }else if (result == "password_error") {
+            } else if (result == "password_error") {
                 System.out.println(result);
-            }
-            else {
+            } else {
                 icon = result;
                 System.out.println(icon);
             }
-        }
-        else {
+        } else {
             result = "Wrong!";
         }
 
         JFXSnackbar snackbar = new JFXSnackbar(BottomPane);
 
-        snackbar.fireEvent(new JFXSnackbar.SnackbarEvent(
-                new JFXSnackbarLayout(result,
-                        "CLOSE",
-                        action -> snackbar.close()),
-                Duration.seconds(2), null));
+        snackbar.show(result, 1000);
+    }
+
+
+    public void RegInput(MouseEvent mouseEvent) {
+    }
+
+    public void LoginTab(MouseEvent mouseEvent) {
+        GroupSignup.setVisible(false);
+        GroupLogin.setVisible(true);
+    }
+
+    public void SignupTab(MouseEvent mouseEvent) {
+        GroupLogin.setVisible(false);
+        GroupSignup.setVisible(true);
     }
 }
