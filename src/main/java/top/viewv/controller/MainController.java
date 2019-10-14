33ba0bc30766737280bdc8c995a23e27655451b5
viewv <zxnnet@gmail.com>
2019-10-14 22:28:01 +0800

    Finish Multi Windows, Build 1.0.3

--- src/main/java/top/viewv/controller/MainController.java
+++ src/main/java/top/viewv/controller/MainController.java
@@ -1,18 +1,26 @@
 package top.viewv.controller;
 
 import com.jfoenix.controls.*;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import top.viewv.database.Login;
+import top.viewv.view.StageManager;
 
 
 public class MainController implements Initializable {
@@ -35,6 +43,10 @@ public class MainController implements Initializable {
     Group GroupLogin;
     @FXML
     Button regButton;
+    @FXML
+    Button testBtn;
+
+    private String tranDataToIndex;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -86,5 +98,29 @@ public class MainController implements Initializable {
     public void SignupTab(MouseEvent mouseEvent) {
         GroupLogin.setVisible(false);
         GroupSignup.setVisible(true);
+    }
+
+    public void testSwitch(MouseEvent mouseEvent) throws IOException {
+        Stage stage=new Stage();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("data/NewLogin.fxml")));
+        stage.setTitle("第二个窗口");
+        stage.setScene(new Scene(root));
+        stage.show();
+        //将第二个窗口保存到map中
+        StageManager.STAGE.put("second", stage);
+        //将本窗口保存到map中
+        StageManager.CONTROLLER.put("index", this);
+        //关闭本窗口
+        Stage index = (Stage) testBtn.getScene().getWindow();
+        index.close();
+    }
+
+
+    public String getTranDataToIndex() {
+        return tranDataToIndex;
+    }
+
+    public void setTranDataToIndex(String tranDataToIndex) {
+        this.tranDataToIndex = tranDataToIndex;
     }
 }
