c79d0203768dede210d979beca15e0cafdb64cd0
viewv <zxnnet@gmail.com>
2019-10-16 21:59:34 +0800

    Add Test Sign Up Function.

---
+++ src/main/java/top/viewv/database/Signup.java
@@ -0,0 +1,42 @@
+package top.viewv.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Signup {
+    public String SignupFun(String account_id,String user_id,String password,String rep_password,String avatar){
+        try {
+            Connect connect = new Connect();
+            Connection conn = connect.getConnection();
+            String sql = "select checkregister(?,?,?,?,?)"; //sql function
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1,account_id);
+            preparedStatement.setString(2,user_id);
+            preparedStatement.setString(3,password);
+            preparedStatement.setString(4,rep_password);
+            preparedStatement.setString(5,avatar);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            String result = null;
+            while (resultSet.next()){
+                result = resultSet.getString(1);
+            }
+            conn.close();
+            /*
+            Account Exists
+            Wrong Account Length
+            Wrong Password Length
+            Password Don't Match
+            ----------------------
+            Staff Account Created Successfully
+            Client Account Created Successfully
+             */
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/top/viewv/test/TestConnection.java
+++ src/main/java/top/viewv/test/TestConnection.java
@@ -1,71 +1,17 @@
 package top.viewv.test;
 
-import java.sql.*;
+import top.viewv.database.Signup;
 
 public class TestConnection {
 
-    // MySQL 8.0 以下版本 - JDBC 驱动名及数据库 URL
-    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://rm-wz9sa6hd1097u6rx4bo.mysql.rds.aliyuncs.com:3306/fems";
-
-    // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
-    //static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    //static final String DB_URL = "jdbc:mysql://localhost:3306/RUNOOB?useSSL=false&serverTimezone=UTC";
-
-    // 数据库的用户名与密码，需要根据自己的设置
-    static final String USER = "root";
-    static final String PASS = "Ingram14";
-
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-        try{
-            // 注册 JDBC 驱动
-            Class.forName(JDBC_DRIVER);
-
-            // 打开链接
-            System.out.println("连接数据库...");
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
-            // 执行查询
-            System.out.println(" 实例化Statement对象...");
-            stmt = conn.createStatement();
-            String sql;
-            sql = "SELECT account_id,password FROM account";
-            ResultSet rs = stmt.executeQuery(sql);
-
-            while(rs.next()){
-                // 通过字段检索
-                String id  = rs.getString("account_id");
-                String password = rs.getString("password");
-
-
-                // 输出数据
-                System.out.println("ID: " + id);
-                System.out.println("Password: "+ password);
-
-            }
-            // 完成后关闭
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch(Exception se){
-            // 处理 JDBC 错误
-            se.printStackTrace();
-        }// 处理 Class.forName 错误
-        finally{
-            // 关闭资源
-            try{
-                if(stmt!=null) stmt.close();
-            }catch(SQLException se2){
-            }// 什么都不做
-            try{
-                if(conn!=null) conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }
-        }
-        System.out.println("Goodbye!");
+        Signup signup = new Signup();
+        String account_id = "444";
+        String user_id = "221001";
+        String password = "123456";
+        String rep_password = "123456";
+        String avatar = "~/Pictures/index.png";
+        System.out.println(signup.SignupFun(account_id,user_id,password,rep_password,avatar));
     }
 }
 
