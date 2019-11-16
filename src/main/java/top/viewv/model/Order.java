1b3c7b2899f24ea41ae1383b4c30b02b0613005d
halfeee <qianrui1999@qq.com>
2019-11-16 15:01:36 +0800

    PlaceOrder Fixs

--- src/main/java/top/viewv/model/Order.java
+++ src/main/java/top/viewv/model/Order.java
@@ -30,43 +30,8 @@ public class Order {
         }
     }
 
-    public void GetMoney(Connection conn){
-        try{
-            String sql = null;
-            double sum = 0.00;
-            Iterator iter = Ingres.entrySet().iterator();
-            while(iter.hasNext()){
-                Map.Entry entry = (Map.Entry)iter.next();
-                int x = Integer.parseInt(entry.getKey().toString());
-                int y = Integer.parseInt(entry.getValue().toString());
 
-                sql = "select product_price from product where product_id = " + x;
-                PreparedStatement st = conn.prepareStatement(sql);
-                ResultSet rs = st.executeQuery();
-                rs.next();
-                sum += rs.getDouble(1);
-
-                sql = "insert into order_product(amount,product_id,order_id) values(" + y + "," + x + "," + this.order_id + ")" ;
-                st.execute(sql);
-            }
-
-            int credit;
-            sql = "select credit from client where client_id = " + this.client_id;
-            PreparedStatement st = conn.prepareStatement(sql);
-            ResultSet rs = st.executeQuery();
-            rs.next();
-            credit = rs.getInt(1);
-            //to do credit
-
-            this.deposit = (float)(0.3 * sum);
-            this.retainage = (float)(0.7 * sum);
-
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
+    //用户下达订单
     public void PlaceOrder(Connection conn, HashMap mess,int days,String client_id){
         try{
             this.situation = 0;
@@ -80,15 +45,64 @@ public class Order {
             System.out.println(utilDate);
             this.due_time = new java.sql.Date(utilDate.getTime());
 
-            String sql = "";
-            PreparedStatement st = conn.prepareStatement(sql);
-            ResultSet rs = st.executeQuery();
+
+            String sql;
+            PreparedStatement st;
+            ResultSet rs;
+            sql = "select count(order_id) from `order`";
+            st = conn.prepareStatement(sql);
+            rs = st.executeQuery();
+            rs.next();
+            this.order_id = rs.getInt(1) + 1;
+            sql = "insert into order(client_id,order_time,due_time,situation) values(" +
+            this.client_id + "," +
+            this.order_time + "," +
+            this.due_time + "," +
+            this.situation + ")";
+            st.execute(sql);
+
+
+
+
+
+            double sum = 0.00;
+            Iterator iter = Ingres.entrySet().iterator();
+            while(iter.hasNext()){
+                Map.Entry entry = (Map.Entry)iter.next();
+                int x = Integer.parseInt(entry.getKey().toString());
+                int y = Integer.parseInt(entry.getValue().toString());
+
+                sql = "select product_price from product where product_id = " + x;
+                st = conn.prepareStatement(sql);
+                rs = st.executeQuery();
+                rs.next();
+                sum += rs.getDouble(1);
+
+                sql = "insert into order_product(amount,product_id,order_id) values(" + y + "," + x + "," + this.order_id + ")" ;
+                st.execute(sql);
+            }
+
+            int credit;
+            sql = "select credit from client where client_id = " + this.client_id;
+            st = conn.prepareStatement(sql);
+            rs = st.executeQuery();
+            rs.next();
+            credit = rs.getInt(1);
+            //to do credit
+
+            this.deposit = (float)(0.3 * sum);
+            this.retainage = (float)(0.7 * sum);
+            sql = "update order set deposit = " + this.deposit + "where order_id = " +
+                    this.order_id;
+            st.execute(sql);
+            sql = "update order set retainage = " + this.retainage + "where order_id = " +
+                    this.order_id;
+            st.execute(sql);
         }
         catch(Exception e){
             e.printStackTrace();
         }
     }
-    //用户下达订单
 
 
     public void AcceptOrder(Connection conn,String staff_id){
