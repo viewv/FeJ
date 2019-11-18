ac15a226e1309f953c26d6c4e561c650040b57d6
halfeee <qianrui1999@qq.com>
2019-11-18 11:00:47 +0800

    Get Product Message By ID

--- src/main/java/top/viewv/model/Product.java
+++ src/main/java/top/viewv/model/Product.java
@@ -1,5 +1,9 @@
 package top.viewv.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Product {
     public int product_id;
     public String product_name;
@@ -13,5 +17,23 @@ public class Product {
         this.product_period = period;
         this.product_price = price;
         this.description = description;
+    }
+
+    public void GetProduct(Connection conn,int id){
+        try{
+            String sql = "select * from product where product_id =" +
+                    id;
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            rs.next();
+            this.product_id = id;
+            this.product_name = rs.getString(2);
+            this.product_period = rs.getInt(3);
+            this.product_price = rs.getFloat(4);
+            this.description = rs.getString(5);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
