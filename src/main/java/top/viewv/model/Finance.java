cc92dcca8a6396621ee4a48df187fc9819b5f427
halfeee <qianrui1999@qq.com>
2019-11-24 16:35:15 +0800

    Lots OF module complement Finance & Storage

--- src/main/java/top/viewv/model/Finance.java
+++ src/main/java/top/viewv/model/Finance.java
@@ -1,9 +1,47 @@
 package top.viewv.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Finance {
-    private int id;
-    private String type;
-    private int order_id;
-    private int amount;
-    private int staff_id;
+    public int id;
+    public int type;
+    public int order_id;
+    public float money;
+    public String staff_id;
+
+//type = (1.订单收入 2.退款支出)
+
+    public int InCome(Connection conn, int OrderID, String staff_id) {
+        try {
+            String sql = "select Income(?,?)";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1,OrderID);
+            st.setString(2,staff_id);
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            return rs.getInt(1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int OutCome(Connection conn, int OrderID,String staff_id){
+        try{
+            String sql = "select Outcome(?,?)";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1,OrderID);
+            st.setString(2,staff_id);
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            return rs.getInt(1);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
--- src/main/java/top/viewv/model/Product_storage.java
+++ src/main/java/top/viewv/model/Product_storage.java
@@ -13,6 +13,7 @@ public class Product_storage {
     public int plan_id;
     public java.sql.Date product_time;
 
+
     public int EnStorage1(Connection conn,int product_id,int amount,String staff_id,int workshop_id,int plan_id){
         try{
             this.product_id = product_id;
@@ -43,6 +44,17 @@ public class Product_storage {
         return 0;
     }
 
+    public void EnStorage2(Connection conn,int OrderID){
+        try{
+            String sql = "update product_storage set order_id = 0 where order_id = " +
+                    OrderID;
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.executeQuery(sql);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
     //To Do
     public void OutStorage(Connection conn,int OrderId,int ProductId,int amount){
         try{
@@ -51,6 +63,49 @@ public class Product_storage {
             st.setInt(1,OrderId);
             st.setInt(2,ProductId);
             st.setInt(3,amount);
+            st.execute(sql);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public int[] CheckDestroy(Connection conn){
+        try{
+            String sql = "select count(product_storage.id) from product_storage,product where " +
+                    "product_storage.product_id = product.product_id and" +
+                    " (product_period-TIMESTAMPDIFF(DAY,product_time,CURRENT_DATE)) < 0" +
+                    "and order_id = 0";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.executeQuery(sql);
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            int row = rs.getInt(1);
+            int[] temp = new int[row];
+            int cnt = 0;
+
+            sql = "select product_storage.id from product_storage,product where " +
+                    "product_storage.product_id = product.product_id and" +
+                    " (product_period-TIMESTAMPDIFF(DAY,product_time,CURRENT_DATE)) < 0" +
+                    "and order_id = 0";
+            st = conn.prepareStatement(sql);
+            st.executeQuery(sql);
+            while(rs.next()){
+                temp[cnt] = rs.getInt(1);
+                cnt++;
+            }
+            return temp;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void DestroyItem(Connection conn,int id){
+        try{
+            String sql = "delete from product_storage where id = " + id;
+            PreparedStatement st = conn.prepareStatement(sql);
             st.execute(sql);
         }
         catch(Exception e){
