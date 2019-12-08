5fde1b138ccb89b63820aeecd7fe744857bc756f
halfeee <qianrui1999@qq.com>
2019-12-08 19:02:14 +0800

    Show All Finance

--- src/main/java/top/viewv/model/Finance.java
+++ src/main/java/top/viewv/model/Finance.java
@@ -11,6 +11,14 @@ public class Finance {
     public float money;
     public String staff_id;
 
+    public Finance(int a,int b,int c,float d,String e){
+        this.id = a;
+        this.type = b;
+        this.order_id = c;
+        this.money = d;
+        this.staff_id = e;
+    }
+
 //type = (1.订单收入 2.退款支出)
 
     public int InCome(Connection conn, int OrderID, String staff_id) {
@@ -41,5 +49,34 @@ public class Finance {
             e.printStackTrace();
         }
         return 0;
+    }
+
+    public Finance[] GetAll(Connection conn){
+        try{
+            String sql = "select count(id) from Finance";
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            rs.next();
+            int cnt = rs.getInt(1);
+            Finance[] X = new Finance[cnt];
+            cnt = 0;
+
+            sql = "select * from Finance";
+            st = conn.prepareStatement(sql);
+            rs = st.executeQuery(sql);
+            while(rs.next()){
+                X[cnt] = new Finance(rs.getInt(1),
+                                     rs.getInt(2),
+                                     rs.getInt(3),
+                                     rs.getFloat(4),
+                                     rs.getString(5));
+                cnt++;
+            }
+            return X;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
