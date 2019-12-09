49a01e8120c8733e69868e457ae1c837bfe2ffde
halfeee <qianrui1999@qq.com>
2019-12-09 23:45:15 +0800

    Ingre & Product Changes

--- src/main/java/top/viewv/model/Ingredient.java
+++ src/main/java/top/viewv/model/Ingredient.java
@@ -1,5 +1,10 @@
 package top.viewv.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Ingredient {
     public int ingredient_id;
     public String ingredient_name;
@@ -12,4 +17,58 @@ public class Ingredient {
         this.ingredient_period =period;
         this.ingredient_cost = cost;
     }
+
+    public Ingredient[] GetAll(Connection conn){
+        try{
+            String sql = "select count(*) from ingredient";
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            int cnt = rs.getInt(1);
+
+            Ingredient[] X = new Ingredient[cnt];
+            cnt = 0;
+            sql = "select * from ingredient";
+            st = conn.prepareStatement(sql);
+            rs = st.executeQuery(sql);
+            while(rs.next()){
+                X[cnt] = new Ingredient(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getInt(3),
+                        rs.getFloat(4)
+                );
+                cnt++;
+            }
+            return X;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public int AddIngredient(Connection conn,String name,int period,float price){
+        try {
+            String sql = "select AddIngredient(\"" +
+                    name + "\"," + period + "," + price + ")";
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            return rs.getInt(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    //1.price error 2.period error 3.success
+
+    public int DeleteIngredient(Connection conn,int id){
+        try{
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    //1.not exist 2.success
 }
