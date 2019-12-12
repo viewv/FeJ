a81d37e8fa14fbe688ddae42f975ce21f5df705f
halfeee <qianrui1999@qq.com>
2019-12-12 16:45:49 +0800

    name api

---
+++ src/main/java/top/viewv/api/Name.java
@@ -0,0 +1,35 @@
+package top.viewv.api;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Name {
+    public static String GetPname(Connection conn,int pid){
+        try{
+            String sql = "select product_name from product where product_id = " + pid;
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            return rs.getString(1);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String GetIname(Connection conn,int iid){
+        try{
+            String sql = "select ingredient_name from product where ingredient_id = " + iid;
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            return rs.getString(1);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
