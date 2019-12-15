4df993881d1e50ec40dd7e39bb8adc89b191d410
viewv <zxnnet@gmail.com>
2019-12-15 13:32:05 +0800

    Failed To Fix file problem

--- src/main/java/top/viewv/api/Serialize.java
+++ src/main/java/top/viewv/api/Serialize.java
@@ -11,41 +11,30 @@ import java.util.Objects;
 
 public class Serialize {
     public static void ser(Object object,String filename) throws Exception {
-        String path =  Objects.requireNonNull(Thread.currentThread().
-                getContextClassLoader().
-                getResource("data/file/"+filename)).getPath();
-        FileOutputStream fos = new FileOutputStream(path);
+        FileOutputStream fos = new FileOutputStream(filename);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(object);
         oos.flush();
         oos.close();
     }
 
-
     public static HashMap dSer(String filename) throws Exception {
-        String path =  Objects.requireNonNull(Thread.currentThread().
-                getContextClassLoader().
-                getResource("data/file/"+filename)).getPath();
-        FileInputStream fis = new FileInputStream(path);
+        FileInputStream fis = new FileInputStream(filename);
         ObjectInputStream ois = new ObjectInputStream(fis);
         HashMap hashMap = (HashMap) ois.readObject();
         return hashMap;
     }
 
     public static ConnectInfo connder (String filename) throws IOException, ClassNotFoundException {
-        String path =  Objects.requireNonNull(Thread.currentThread().
-                getContextClassLoader().
-                getResource("data/file/"+filename)).getPath();
-        FileInputStream fis = new FileInputStream(path);
+        FileInputStream fis = new FileInputStream(filename);
+
         ObjectInputStream ois = new ObjectInputStream(fis);
         return (ConnectInfo) ois.readObject();
     }
 
     public static ArrayList<PlanInfo> planInfodser (String filename) throws IOException, ClassNotFoundException {
-        String path =  Objects.requireNonNull(Thread.currentThread().
-                getContextClassLoader().
-                getResource("data/file/"+filename)).getPath();
-        FileInputStream fis = new FileInputStream(path);
+        FileInputStream fis = new FileInputStream(filename);
+
         ObjectInputStream ois = new ObjectInputStream(fis);
         return (ArrayList<PlanInfo>) ois.readObject();
     }
